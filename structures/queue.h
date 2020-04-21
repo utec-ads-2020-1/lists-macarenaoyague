ffe0534f6679d58c5982a5bf9767e86118ec3dd3
Macarena <macarena.oyague@utec.edu.pe>
2020-04-21 00:53:41 -0500

    Stack and queue

--- structures/queue.h
+++ structures/queue.h
@@ -1,6 +1,8 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stdexcept>
+
 using namespace std;
 
 #define MAX 1000 
@@ -16,13 +18,74 @@ public:
     queue(int size = MAX);
     ~queue();
 
-    void push(T);
+    void push(T data);
     void pop();
     T front();
     T back();
 
 	int size();
 	bool empty();
+
+    void duplicateCapacity();
 };
+
+template<typename T>
+queue<T>::queue(int size) {
+    capacity = size;
+    T arr[capacity] = {};
+    this->data = arr;
+    top = 0;
+}
+
+template<typename T>
+void queue<T>::push(T data) {
+    if (top == capacity)
+        duplicateCapacity();
+    this->data[top] = data;
+    top++;
+}
+
+template<typename T>
+void queue<T>::pop() {
+    if (empty())
+        throw std::out_of_range("Empty queue");
+    else
+    {
+        top--;
+        for (int i = 0; i<top; i++)
+            this->data[i] = this->data[i+1];
+        this->data[top] = 0;
+    }
+}
+
+template<typename T>
+T queue<T>::front() {
+   // return this->data[];
+}
+
+template<typename T>
+T queue<T>::back() {
+    //
+    return nullptr;
+}
+
+template<typename T>
+int queue<T>::size() {
+    return top;
+}
+
+template<typename T>
+bool queue<T>::empty() {
+    return size() == 0;
+}
+
+template<typename T>
+void queue<T>::duplicateCapacity(){
+    capacity *= 2;
+    T array [capacity] = {};
+    for (int i = 0; i<capacity/2; i++)
+        array [i] = this->data[i];
+    this->data = array;
+}
 
 #endif
--- structures/stack.h
+++ structures/stack.h
@@ -1,6 +1,8 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <stdexcept>
+
 using namespace std;
 
 #define MAX 1000 
@@ -22,40 +24,71 @@ public:
 
 	int size();
 	bool empty();
+
+    void duplicateCapacity();
 };
-
-
 
 
 template<typename T>
 stack<T>::stack(int size) {
-
+    capacity = size;
+    T arr [capacity] = {};
+    data = arr;
+    top = 0;
 }
 
 template<typename T>
 void stack<T>::push(T data) {
-
+    if (top == capacity-1)
+        duplicateCapacity();
+    this->data[top] = data;
+    top++;
 }
 
 template<typename T>
 void stack<T>::pop() {
-
+    if (top == 0)
+        throw std::out_of_range ("Empty stack");
+    else
+    {
+        top--;
+        this->data[top] = 0;
+    }
 }
 
 template<typename T>
 T stack<T>::peak() {
-    return nullptr;
+    if (top == 0)
+        throw std::out_of_range ("Empty stack");
+    else
+        return this->data[top];
 }
 
 template<typename T>
 int stack<T>::size() {
-    return 0;
+    return top;
 }
 
 template<typename T>
 bool stack<T>::empty() {
-    return false;
+    if (top == 0)
+        return true;
+    else
+        return false;
 }
 
+template<typename T>
+void stack<T>::duplicateCapacity(){
+    capacity *= 2;
+    T array [capacity] = {};
+    for (int i = 0; i<capacity/2; i++)
+        array [i] = this->data[i];
+    this->data = array;
+}
+
+template<typename T>
+stack<T>::~stack() {
+    delete this->data;
+}
 
 #endif
