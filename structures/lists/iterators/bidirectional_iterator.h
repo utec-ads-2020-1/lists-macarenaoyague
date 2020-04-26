0a60962d1e2215792f1b8c0d0e6654f8b87c2b00
Macarena <macarena.oyague@utec.edu.pe>
2020-04-26 16:41:16 -0500

    Update Iterators

--- structures/lists/iterators/bidirectional_iterator.h
+++ structures/lists/iterators/bidirectional_iterator.h
@@ -11,11 +11,11 @@ class BidirectionalIterator {
 
     public:
         BidirectionalIterator();
-        BidirectionalIterator(Node<T>*);
+        BidirectionalIterator(Node<T>* node);
 
-        BidirectionalIterator<T> operator=(BidirectionalIterator<T>);
+        BidirectionalIterator<T> operator=(BidirectionalIterator<T> iterator);
 
-        bool operator!=(BidirectionalIterator<T>);
+        bool operator!=(BidirectionalIterator<T> iterator);
 
         BidirectionalIterator<T> operator++();
 
@@ -23,5 +23,43 @@ class BidirectionalIterator {
 
         T operator*();
 };
+
+template<typename T>
+BidirectionalIterator<T>::BidirectionalIterator() {
+    current = nullptr;
+}
+
+template<typename T>
+BidirectionalIterator<T>::BidirectionalIterator(Node<T> * node) {
+    current = node;
+}
+
+template<typename T>
+BidirectionalIterator<T> BidirectionalIterator<T>::operator=(BidirectionalIterator<T> iterator) {
+    return BidirectionalIterator<T>(iterator.current);
+}
+
+template<typename T>
+bool BidirectionalIterator<T>::operator!=(BidirectionalIterator<T> iterator) {
+    return current!=iterator.current;
+}
+
+template<typename T>
+BidirectionalIterator<T> BidirectionalIterator<T>::operator++() {
+    if (current == nullptr)
+        throw std::out_of_range ("Pointing to null. There is no next iterator.");
+    return BidirectionalIterator<T>(current->next);
+}
+
+template<typename T>
+BidirectionalIterator<T> BidirectionalIterator<T>::operator--() {
+
+    return BidirectionalIterator<T>();
+}
+
+template<typename T>
+T BidirectionalIterator<T>::operator*() {
+    return nullptr;
+}
 
 #endif
--- structures/lists/iterators/forward_iterator.h
+++ structures/lists/iterators/forward_iterator.h
@@ -11,15 +11,49 @@ class ForwardIterator {
 
     public:
         ForwardIterator();
-        ForwardIterator(Node<T>*);
+        ForwardIterator(Node<T>* node);
 
-        ForwardIterator<T> operator=(ForwardIterator<T>);
+        ForwardIterator<T> operator=(ForwardIterator<T> iterator);
 
-        bool operator!=(ForwardIterator<T>);
+        bool operator!=(ForwardIterator<T> iterator);
 
         ForwardIterator<T> operator++();
 
         T operator*();
 };
+
+template<typename T>
+ForwardIterator<T>::ForwardIterator() {
+    current = nullptr;
+}
+
+template<typename T>
+ForwardIterator<T>::ForwardIterator(Node<T> * node) {
+    current = node;
+}
+
+template<typename T>
+ForwardIterator<T> ForwardIterator<T>::operator=(ForwardIterator<T> iterator) {
+    return ForwardIterator<T>(iterator.current);
+}
+
+template<typename T>
+bool ForwardIterator<T>::operator!=(ForwardIterator<T> iterator) {
+    return current != iterator.current;
+}
+
+template<typename T>
+ForwardIterator<T> ForwardIterator<T>::operator++() {
+    if (current == nullptr)
+        throw std::out_of_range ("Pointing to null. There is no next iterator.");
+    return ForwardIterator<T>(current->next);
+}
+
+template<typename T>
+T ForwardIterator<T>::operator*() {
+    if (current == nullptr)
+        throw std::out_of_range ("Pointing to null. There is no data.");
+    return current->data;
+}
 
 #endif
